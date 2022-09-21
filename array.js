3e869e13149b79e1f795b0d78befd8016e3ee674
Craig Weller <crg.weller@gmail.com>
2022-09-21 11:58:14 -0600

    Include method calls

--- array.js
+++ array.js
@@ -1,3 +1,15 @@
 var people = ["Alice", "Brandon", "Cindy", "Debbie", "Ernie"];
 var ages = [32, 43, 54, 65,76]
 var inDenver = [true, false, true, false, true]
+console.log(people)
+console.log(ages)
+console.log(inDenver)
+//This will remove the last element ("Ernie") from the array
+people.pop()
+console.log(people)
+//This will remove the first element (32) from the array
+ages.shift()
+console.log(ages)
+//This would add new elements to the end of the array, but I don't have any to add so it won't do anything
+inDenver.push()
+console.log(inDenver)
